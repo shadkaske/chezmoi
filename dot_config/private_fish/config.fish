3652735aa75454fb7eceafdc9ddd1bf0dc55c88e
Shad Kaske <shadkaske@gmail.com>
2023-12-14 13:20:55 -0600

    Added abbreviations and color theme chooser

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -9,6 +9,16 @@ set --universal pure_enable_single_line_prompt true
 
 set -Ux EDITOR nvim
 
+abbr --add czcd cd (chezmoi source-path)
+abbr --add czed chezmoi edit
+abbr --add czap chezmoi apply
+abbr --add czdf chezmoi diff
+abbr --add czad chezmoi add
+
+abbr --add lg lazygit
+
+fish_config theme choose "Catppuccin Mocha"
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
