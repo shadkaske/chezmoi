4fca4371b63c22f96390adaf9790a075b7412e0d
Shad Kaske <shadkaske@gmail.com>
2023-12-14 13:21:10 -0600

    Updated run once script

--- run_once_before_install_fisher.sh
+++ run_once_before_install_fisher.sh
@@ -3,3 +3,7 @@
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source
 
 fisher install pure-fish/pure
+
+fisher install catppuccin/fish
+
+fish_config theme choose "Catppuccin Mocha"
