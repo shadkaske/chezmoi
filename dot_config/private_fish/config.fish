19e43cb14531ac297dffa66f7020d1349da2cd91
Shad Kaske <shadkaske@gmail.com>
2023-12-15 14:07:27 -0600

    starship prompt for Fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -25,6 +25,8 @@ abbr --add artisan php artisan
 
 fish_config theme choose "Catppuccin Mocha"
 
+starship init fish | source
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
