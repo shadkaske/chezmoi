428183425fa71a5de2197aa103d8b0b528567c20
Shad Kaske <shadkaske@gmail.com>
2023-12-21 06:10:48 -0600

    Fix la function to actually list all

--- dot_config/private_fish/functions/la.fish
+++ dot_config/private_fish/functions/la.fish
@@ -1,6 +1,6 @@
 function la -d "List all directory contents - long"
     if command -q eza
-        eza --icons --group --header --group-directories-first --long $argv
+        eza --icons --group --header --group-directories-first --long --all $argv
     else
         ls -alh $argv
     end
